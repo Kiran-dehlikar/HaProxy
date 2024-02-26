394601ce432b95635498b292e6a1b8e043a3561e
kdrocks <104997588+Kiran-dehlikar@users.noreply.github.com>
2024-02-27 00:58:14 +0530

    Update main.tf

--- infra/loadbalancer/main.tf
+++ infra/loadbalancer/main.tf
@@ -16,5 +16,15 @@ resource "aws_lb_target_group_attachment" "my_attachment" {
   target_id        = var.ha_instance_id[count.index]
   port             = var.ha_lb_port  
 }
+# Add listeners 
+resource "aws_lb_listener" "my_listener_http" {
+  load_balancer_arn = aws_lb.my_load_balancer.arn
+  port              = var.ha_lb_port
+  protocol          = "HTTP"
 
+  default_action {
+    type             = "forward"
+    target_group_arn = var.ha_tg_arn
+  }
+}
 
