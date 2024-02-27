8b30fb0a377689cf043ee7476734848c7890d5dd
kdrocks <104997588+Kiran-dehlikar@users.noreply.github.com>
2024-02-27 18:31:02 +0530

    Update output.tf

--- infra/subnet/output.tf
+++ infra/subnet/output.tf
@@ -6,3 +6,7 @@ output "public_subnet_out" {
 output "private_subnet_out" {
   value = aws_subnet.private_subnets[*].id
 }
+# Get the second value of private subnets from us-east-1b az
+output "us-east-1b_az_subnet_id" {
+  value = aws_subnet.private_subnets[1].id
+}
